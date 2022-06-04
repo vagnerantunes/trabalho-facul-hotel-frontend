9b3d87f63bc8c38b3a5fec83d01c16ed663ced5d
Vagner Antunes <vagner.antunes0701@outlook.com>
2022-06-04 19:26:16 -0300

    exclui anotações na classe dropdown

--- src/app/components/funcionario/dropdown.service.ts
+++ src/app/components/funcionario/dropdown.service.ts
@@ -21,28 +21,4 @@ export class DropdownService {
     return this.http.post<Cargo>(this.baseUrl, Cargo)
   }
 
-  /*
-  create(Cargo: Cargo): Observable<Cargo>{
-    return this.http.post<Cargo>(this.baseUrl, Cargo)
-  }
-
-  read(): Observable<Cargo[]>{
-    return this.http.get<Cargo[]>(this.baseUrl)
-  }
-
-  readById(codigo: string): Observable<Cargo>{
-    const url = `${this.baseUrl}/${codigo}`
-    return this.http.get<Cargo>(url)
-  }
- 
-  update(cargo: Cargo): Observable<Cargo>{
-    const url = `${this.baseUrl}/${cargo.codigo}`
-    return this.http.put<Cargo>(url, cargo)
-  }
-  
-  delete(codigo: number): Observable<Cargo>{    
-    const url = `${this.baseUrl}/${codigo}`
-    return this.http.delete<Cargo>(url)
-  }
-  */
 }
